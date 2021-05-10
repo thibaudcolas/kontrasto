d69a72538631e909bf940e45204a3babaedefc41
Thibaud Colas <thibaudcolas@gmail.com>
2021-05-10 22:56:11 +0100

    Project cleanup

--- demo/settings.py
+++ demo/settings.py
@@ -134,11 +134,11 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-DJANGO_VITE_ASSETS_PATH = os.path.join(PROJECT_DIR, "static_vite")
+DJANGO_VITE_ASSETS_PATH = os.path.join(PROJECT_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static"),
     DJANGO_VITE_ASSETS_PATH,
+    os.path.join(PROJECT_DIR, "favicons"),
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
@@ -172,7 +172,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DEBUG = True
 
 DJANGO_VITE_DEV_MODE = True
-DJANGO_VITE_MANIFEST_PATH = os.path.join(DJANGO_VITE_ASSETS_PATH, "manifest.json")
+DJANGO_VITE_MANIFEST_PATH = os.path.join(
+    DJANGO_VITE_ASSETS_PATH, "manifest.json"
+)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "x*@ouu6*vj*i!_-6x*@_yklrs5f4swz2(x*+dlzdaf7@7go_d-"
